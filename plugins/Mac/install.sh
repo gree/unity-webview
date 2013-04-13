6ad852381ded0c51352a91480a7392b4266364f0
Nobuki Yoda <nobuki.y@gmail.com>
2013-04-14 06:04:11 +0900

    Modify build scripts.

--- plugins/Mac/install.sh
+++ plugins/Mac/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 DSTDIR="../../build/Packager/Assets/Plugins"
 rm -rf DerivedData
-xcodebuild -scheme WebView -configuration Release build
+xcodebuild -scheme WebView -configuration Release build CONFIGURATION_BUILD_DIR='DerivedData'
 mkdir -p $DSTDIR
-cp -r DerivedData/WebView/Build/Products/Release/WebView.bundle $DSTDIR
+cp -r DerivedData/WebView.bundle $DSTDIR
 rm -rf DerivedData
