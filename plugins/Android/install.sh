fb2277bb792f055466598a0a2bf5b0b010eb4a51
Koji Nakamaru <koji.nakamaru@gree.net>
2015-08-31 17:53:28 +0900

    fixed plugins/Android/install.sh to work with unity 5 and to set target android version explicitly.

--- plugins/Android/install.sh
+++ plugins/Android/install.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 UNITYLIBS="/Applications/Unity/Unity.app/Contents/PlaybackEngines/AndroidPlayer/bin/classes.jar"
+if [ ! -f $UNITYLIBS ]
+then
+    UNITYLIBS="/Applications/Unity/Unity.app/Contents/PlaybackEngines/AndroidPlayer/release/bin/classes.jar"
+fi
 DSTDIR="../../build/Packager/Assets/Plugins/Android"
 export ANT_OPTS=-Dfile.encoding=UTF8
-android update project -t 1 -p .
+android update project -t android-18 -p .
 mkdir -p libs
 cp $UNITYLIBS libs
 ant release
