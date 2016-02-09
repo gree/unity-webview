b3dd5623de6047f35103ba568f8301406787f55b
Koji Nakamaru <koji.nakamaru@gree.net>
2016-02-09 11:29:35 +0900

    modified plugins/Mac/install.sh to place the osx plugin under Plugins/x86_64/ instead of Plugins/.

--- plugins/Mac/install.sh
+++ plugins/Mac/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-DSTDIR="../../build/Packager/Assets/Plugins"
+DSTDIR="../../build/Packager/Assets/Plugins/x86_64"
 rm -rf DerivedData
 xcodebuild -scheme WebView -configuration Release build CONFIGURATION_BUILD_DIR='DerivedData'
 mkdir -p $DSTDIR
