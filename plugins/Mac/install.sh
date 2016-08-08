3b7a4e046e4e11a22880e388dcdad0472597d174
Koji Nakamaru <koji.nakamaru@gree.net>
2016-08-08 12:43:31 +0900

    fixed Rakefile and Mac/install.sh.

--- plugins/Mac/install.sh
+++ plugins/Mac/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-DSTDIR="../../build/Packager/Assets/Plugins/x86_64"
+DSTDIR="../../build/Packager/Assets/Plugins"
 rm -rf DerivedData
 xcodebuild -scheme WebView -configuration Release -arch i386 -arch x86_64 build CONFIGURATION_BUILD_DIR='DerivedData'
 mkdir -p $DSTDIR
