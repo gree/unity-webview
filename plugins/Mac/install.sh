36ad915db58459d6f52d815b5e219beb51a24a90
Koji Nakamaru <koji.nakamaru@gree.net>
2017-09-19 23:15:32 +0900

    experimental support for .NET 4.6 runtime.

--- plugins/Mac/install.sh
+++ plugins/Mac/install.sh
@@ -5,13 +5,13 @@ xcodebuild -target WebView -configuration Release -arch i386 -arch x86_64 build 
 xcodebuild -target WebViewSeparated -configuration Release -arch x86_64 build CONFIGURATION_BUILD_DIR='DerivedData'
 mkdir -p $DSTDIR
 
+
 # adjust libmono.0.dylib paths
 for i in WebView WebViewSeparated
 do
     pushd DerivedData/$i.bundle/Contents/MacOS
-    install_name_tool -change @executable_path/../Frameworks/MonoEmbedRuntime/osx/libmono.0.dylib @rpath/libmono.0.dylib $i
-    install_name_tool -add_rpath @executable_path/../Frameworks/MonoEmbedRuntime/osx/ $i
-    install_name_tool -add_rpath @executable_path/../Frameworks/Mono/MonoEmbedRuntime/osx/ $i
+    install_name_tool -change @executable_path/../Frameworks/MonoEmbedRuntime/osx/libmonobdwgc-2.0.dylib @rpath/libmonobdwgc-2.0.dylib $i
+    install_name_tool -add_rpath @executable_path/../Frameworks/MonoBleedingEdge/MonoEmbedRuntime/osx/ $i
     popd
 done
 
