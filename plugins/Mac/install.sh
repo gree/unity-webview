60d028181e8dfb7dea961e7e2e87ced9eace480c
岡安正順 <okayasu@bexide.co.jp>
2018-03-23 12:53:07 +0900

    Modify unity mono library path when install.sh running. check unity version and update linker option on WebView.xcodeproj

--- plugins/Mac/install.sh
+++ plugins/Mac/install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 DSTDIR="../../build/Packager/Assets/Plugins"
+UNITYAPP="/Applications/Unity/Unity.app"
+version=`/usr/libexec/PlistBuddy -c "Print CFBundleVersion" ${UNITYAPP}/Contents/Info.plist`
+if [ `echo $version | sed -e "s/\..*$//"` = "2017" ] ; then
+    sed -i "" -e "s@/Applications/Unity/Unity.app/Contents/Frameworks/MonoEmbedRuntime/osx@${UNITYAPP}/Contents/Frameworks/Mono/MonoEmbedRuntime/osx@g" WebView.xcodeproj/project.pbxproj
+else
+    sed -i "" -e "s@/Applications/Unity/Unity.app/Contents/Frameworks/MonoEmbedRuntime/osx@${UNITYAPP}/Contents/Frameworks/MonoEmbedRuntime/osx@g" WebView.xcodeproj/project.pbxproj
+fi
 rm -rf DerivedData
 xcodebuild -target WebView -configuration Release -arch i386 -arch x86_64 build CONFIGURATION_BUILD_DIR='DerivedData'
 xcodebuild -target WebViewSeparated -configuration Release -arch x86_64 build CONFIGURATION_BUILD_DIR='DerivedData'
