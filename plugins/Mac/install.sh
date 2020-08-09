87f0a96d220be22bb7fe9e565b059ba8de57289c
Koji Nakamaru <koji.nakamaru@gree.net>
2020-08-09 22:10:43 +0900

    updated the project file.

--- plugins/Mac/install.sh
+++ plugins/Mac/install.sh
@@ -2,10 +2,8 @@
 DSTDIR="../../build/Packager/Assets/Plugins"
 rm -rf DerivedData
 xcodebuild -target WebView -configuration Release -arch i386 -arch x86_64 build CONFIGURATION_BUILD_DIR='DerivedData' | xcpretty
-xcodebuild -target WebViewSeparated -configuration Release -arch x86_64 build CONFIGURATION_BUILD_DIR='DerivedData' | xcpretty
 mkdir -p $DSTDIR
 
 cp -r DerivedData/WebView.bundle $DSTDIR
-cp -r DerivedData/WebViewSeparated.bundle $DSTDIR
 rm -rf DerivedData
 cp *.bundle.meta $DSTDIR
