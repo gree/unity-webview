e12b9dca8ca6adf3b8f4a6c44a03039f60f69870
Lavi Kabel <lavi@slatescience.com>
2022-06-02 17:49:56 +0300

    Revert CUnityPlayerAcitivity somehow it appers deleted on our branch in the merge from gree

--- plugins/Android/webview/src/main/java/net/gree/unitywebview/CUnityPlayerActivity.java
+++ plugins/Android/webview/src/main/java/net/gree/unitywebview/CUnityPlayerActivity.java
@@ -1,18 +1,152 @@
 package net.gree.unitywebview;
 
-import com.unity3d.player.*;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.os.Process;
+import com.unity3d.player.*;
+//Matific NOTE:
+//This is the Activity as exported from Unity as an Android project
+//+ Expose PauseUnityPlayer/ResumeUnityPlayer + some logic
 
-public class CUnityPlayerActivity
-    extends UnityPlayerActivity
+
+public class CUnityPlayerActivity extends Activity implements IUnityPlayerLifecycleEvents
 {
-    @Override
-    public void onCreate(Bundle bundle) {
-        requestWindowFeature(1);
-        super.onCreate(bundle);
+    public static CUnityPlayerActivity GlobalUnityActivity = null;
+    protected CUnityPlayer mUnityPlayer; // don't change the name of this variable; referenced from native code
+
+    // Override this in your custom UnityPlayerActivity to tweak the command line arguments passed to the Unity Android Player
+    // The command line arguments are passed as a string, separated by spaces
+    // UnityPlayerActivity calls this from 'onCreate'
+    // Supported: -force-gles20, -force-gles30, -force-gles31, -force-gles31aep, -force-gles32, -force-gles, -force-vulkan
+    // See https://docs.unity3d.com/Manual/CommandLineArguments.html
+    // @param cmdLine the current command line arguments, may be null
+    // @return the modified command line string or null
+    protected String updateUnityCommandLineArguments(String cmdLine)
+    {
+        return cmdLine;
+    }
+
+    boolean mShouldPlayerPause = false;
+    public void PauseUnityPlayer(){
+        mShouldPlayerPause = true;
+        mUnityPlayer.pause();
+    }
+
+    public void ResumeUnityPlayer(){
+        mShouldPlayerPause = false;
+        mUnityPlayer.resume();
+    }
+
+    // Setup activity layout
+    @Override protected void onCreate(Bundle savedInstanceState)
+    {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
         getWindow().setFormat(2);
+
+        String cmdLine = updateUnityCommandLineArguments(getIntent().getStringExtra("unity"));
+        getIntent().putExtra("unity", cmdLine);
+
         mUnityPlayer = new CUnityPlayer(this);
+        CUnityPlayerActivity.GlobalUnityActivity = this;
+
         setContentView(mUnityPlayer);
         mUnityPlayer.requestFocus();
     }
+
+    // When Unity player unloaded move task to background
+    @Override public void onUnityPlayerUnloaded() {
+        moveTaskToBack(true);
+    }
+
+    // Callback before Unity player process is killed
+    @Override public void onUnityPlayerQuitted() {
+    }
+
+    @Override protected void onNewIntent(Intent intent)
+    {
+        // To support deep linking, we need to make sure that the client can get access to
+        // the last sent intent. The clients access this through a JNI api that allows them
+        // to get the intent set on launch. To update that after launch we have to manually
+        // replace the intent with the one caught here.
+        setIntent(intent);
+        mUnityPlayer.newIntent(intent);
+    }
+
+    // Quit Unity
+    @Override protected void onDestroy ()
+    {
+        mUnityPlayer.destroy();
+        super.onDestroy();
+    }
+    // Pause Unity
+    @Override protected void onPause()
+    {
+        super.onPause();
+        mUnityPlayer.pause();
+    }
+
+    // Resume Unity
+    @Override protected void onResume()
+    {
+        super.onResume();
+        if(!mShouldPlayerPause){
+            mUnityPlayer.resume();
+        }
+    }
+
+    // Low Memory Unity
+    @Override public void onLowMemory()
+    {
+        super.onLowMemory();
+        mUnityPlayer.lowMemory();
+    }
+
+    // Trim Memory Unity
+    @Override public void onTrimMemory(int level)
+    {
+        super.onTrimMemory(level);
+        if (level == TRIM_MEMORY_RUNNING_CRITICAL)
+        {
+            mUnityPlayer.lowMemory();
+        }
+    }
+
+    // This ensures the layout will be correct.
+    @Override public void onConfigurationChanged(Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+        mUnityPlayer.configurationChanged(newConfig);
+    }
+
+    // Notify Unity of the focus change.
+    @Override public void onWindowFocusChanged(boolean hasFocus)
+    {
+        super.onWindowFocusChanged(hasFocus);
+        mUnityPlayer.windowFocusChanged(hasFocus);
+    }
+
+    // For some reason the multiple keyevent type is not supported by the ndk.
+    // Force event injection by overriding dispatchKeyEvent().
+    @Override public boolean dispatchKeyEvent(KeyEvent event)
+    {
+        if (event.getAction() == KeyEvent.ACTION_MULTIPLE)
+            return mUnityPlayer.injectEvent(event);
+        return super.dispatchKeyEvent(event);
+    }
+
+    // Pass any events not handled by (unfocused) views straight to UnityPlayer
+    @Override public boolean onKeyUp(int keyCode, KeyEvent event)     { return mUnityPlayer.injectEvent(event); }
+    @Override public boolean onKeyDown(int keyCode, KeyEvent event)   { return mUnityPlayer.injectEvent(event); }
+    @Override public boolean onTouchEvent(MotionEvent event)          { return mUnityPlayer.injectEvent(event); }
+    /*API12*/ public boolean onGenericMotionEvent(MotionEvent event)  { return mUnityPlayer.injectEvent(event); }
 }
