6fb7ed2cad2db6de5bad4a9369cb7fcc75796fa3
Koji Nakamaru <koji.nakamaru@gree.net>
2023-03-11 15:39:04 +0900

    adjusted android code. (cf. c7b487e5eee7de485437f30dc70186802462d844 )

--- plugins/Android/webview/src/main/java/net/gree/unitywebview/CWebViewPlugin.java
+++ plugins/Android/webview/src/main/java/net/gree/unitywebview/CWebViewPlugin.java
@@ -486,7 +486,7 @@ public class CWebViewPlugin extends Fragment {
                     super.onHideCustomView();
                     if (layout != null) {
                         layout.removeView(mVideoView);
-                        layout.setBackgroundColor(0x00000000);
+                        layout.setBackgroundColor(0xff000000);
                         mVideoView = null;
                     }
                 }
@@ -786,6 +786,7 @@ public class CWebViewPlugin extends Fragment {
 
             if (layout == null || layout.getParent() != a.findViewById(android.R.id.content)) {
                 layout = new FrameLayout(a);
+                layout.setBackgroundColor(0xff000000);
                 a.addContentView(
                     layout,
                     new LayoutParams(
@@ -793,6 +794,14 @@ public class CWebViewPlugin extends Fragment {
                         LayoutParams.MATCH_PARENT));
                 layout.setFocusable(true);
                 layout.setFocusableInTouchMode(true);
+                {
+                    // cf. https://stackoverflow.com/questions/6759036/how-to-send-view-to-back-how-to-control-the-z-order-programmatically/19872801#19872801
+                    ViewGroup parent = (ViewGroup)layout.getParent();
+                    if (parent != null) {
+                        parent.removeView(layout);
+                        parent.addView(layout, 0);
+                    }
+                }
             }
             layout.addView(
                 webView,
