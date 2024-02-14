da2d83c2a293e42566f5cc7b34a29b6d53cc7ca5
Koji Nakamaru <koji.nakamaru@gree.net>
2024-02-14 17:30:29 +0900

    added AddCustomHeader() and modified http://httpbin.org/headers -> https://httpbin.org/headers.

--- sample/Assets/Scripts/SampleWebView.cs
+++ sample/Assets/Scripts/SampleWebView.cs
@@ -149,6 +149,7 @@ public class SampleWebView : MonoBehaviour
         webViewObject.SetMargins(5, 100, 5, Screen.height / 4);
         webViewObject.SetTextZoom(100);  // android only. cf. https://stackoverflow.com/questions/21647641/android-webview-set-font-size-system-default/47017410#47017410
         webViewObject.SetVisibility(true);
+        webViewObject.AddCustomHeader("x-auth", "Bearer 0123456789abcdef");
 
 #if !UNITY_WEBPLAYER && !UNITY_WEBGL
         if (Url.StartsWith("http")) {
