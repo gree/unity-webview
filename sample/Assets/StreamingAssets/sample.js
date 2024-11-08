b7824d5f3808d558be20640157456b71abe31c2d
Koji Nakamaru <koji.nakamaru@gree.net>
2024-11-08 13:56:55 +0900

    modified sample.html/sample.js to demonstrate camera/microphone access.

--- sample/Assets/StreamingAssets/sample.js
+++ sample/Assets/StreamingAssets/sample.js
@@ -1,26 +1,16 @@
 window.addEventListener(
     'load',
     function() {
-        document.body.style.backgroundColor = 'white';
-        window.setTimeout(
-            function() {
-                document.body.style.backgroundColor = '#ABEBC6';
-                var msg = document.getElementById("msg");
-                msg.textContent = '(NOTE: the background color was changed by sample.js, for checking whether the external js code works)';
-            },
-            3000);
+        // cf. https://www.aedi.jp/work/webrtc/
+        var constraints = { audio: true, video: { facingMode: "user" } };
+        navigator.mediaDevices
+            .getUserMedia(constraints)
+            .then(function(mediaStream) {
+                var video = document.querySelector('video');
+                video.srcObject = mediaStream;
+                video.onloadedmetadata = function(e) {
+                    video.play();
+                };
+            })
+            .catch(function(err) { console.log(err.name + ": " + err.message); });
     });
-function testSaveDataURL() {
-    var canvas = document.createElement('canvas');
-    canvas.width = 320;
-    canvas.height = 240;
-    var ctx = canvas.getContext("2d");
-    ctx.fillStyle = "#fffaf0";
-    ctx.fillRect(0, 0, 320, 240);
-    ctx.fillStyle = "#000000";
-    ctx.font = "48px serif";
-    ctx.strokeText("Hello, world", 40, 132);
-    Unity.saveDataURL("test.png", canvas.toDataURL());
-    // NOTE: Unity.saveDataURL() for iOS cannot save a file under the common Downloads folder.
-    // cf. https://github.com/gree/unity-webview/pull/904#issue-1650406563
-};
