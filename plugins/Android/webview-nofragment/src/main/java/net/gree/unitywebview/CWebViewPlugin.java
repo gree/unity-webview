60a5a2cb94b413a4bd639b237a507cb64349e487
Koji Nakamaru <koji.nakamaru@gree.net>
2025-05-16 12:38:56 +0900

    modified Android implementation with com.github.rongi.rotate_layout to support rotated webview.

--- plugins/Android/webview-nofragment/src/main/java/net/gree/unitywebview/CWebViewPlugin.java
+++ plugins/Android/webview-nofragment/src/main/java/net/gree/unitywebview/CWebViewPlugin.java
@@ -74,6 +74,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import com.unity3d.player.UnityPlayer;
+import com.github.rongi.rotate_layout.layout.RotateLayout;
 
 class CWebViewPluginInterface {
     private CWebViewPlugin mPlugin;
@@ -208,7 +209,7 @@ public class CWebViewPlugin {
         return mWebView != null;
     }
 
-    public void Init(final String gameObject, final boolean transparent, final boolean zoom, final int androidForceDarkMode, final String ua, final int radius) {
+    public void Init(final String gameObject, final boolean transparent, final boolean zoom, final int androidForceDarkMode, final String ua, final int radius, final int angle) {
         final CWebViewPlugin self = this;
         final Activity a = UnityPlayer.currentActivity;
         if (CWebViewPlugin.isDestroyed(a)) {
@@ -614,12 +615,20 @@ public class CWebViewPlugin {
                 layout.setFocusable(true);
                 layout.setFocusableInTouchMode(true);
             }
+            RotateLayout rotateLayout = new RotateLayout(a);
             layout.addView(
+                rotateLayout,
+                new FrameLayout.LayoutParams(
+                    LayoutParams.MATCH_PARENT,
+                    LayoutParams.MATCH_PARENT,
+                    Gravity.NO_GRAVITY));
+            rotateLayout.addView(
                 webView,
                 new FrameLayout.LayoutParams(
                     LayoutParams.MATCH_PARENT,
                     LayoutParams.MATCH_PARENT,
                     Gravity.NO_GRAVITY));
+            rotateLayout.setAngle(angle);
             mWebView = webView;
         }});
 
@@ -689,7 +698,9 @@ public class CWebViewPlugin {
                 layout.setBackgroundColor(0x00000000);
                 mVideoView = null;
             }
-            layout.removeView(webView);
+            RotateLayout rotateLayout = (RotateLayout)webView.getParent();
+            rotateLayout.removeView(webView);
+            layout.removeView(rotateLayout);
             webView.destroy();
         }});
     }
@@ -817,7 +828,7 @@ public class CWebViewPlugin {
             if (mWebView == null) {
                 return;
             }
-            mWebView.setLayoutParams(params);
+            ((RotateLayout)mWebView.getParent()).setLayoutParams(params);
         }});
     }
 
